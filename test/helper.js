99739dda35070b6d44d7006d8cccc50a482c7c34
Aminesh kumar verma <119420185+AmineshOptisoft@users.noreply.github.com>
2025-08-01 03:37:14 +0530

    feat: add test helper functions for HTTP requests
    - A createTestRequest function for making POST/PUT requests with JSON bodies
    - A createGetRequest function for making GET requests
    - A createDeleteRequest function for making DELETE requests
    - All functions use the servertest library as requested

---
+++ test/helper.js
@@ -0,0 +1,36 @@
+// External dependencies
+const servertest = require('servertest')
+
+// Test helper functions
+function createTestRequest (server, endpoint, method, requestBody, callback) {
+  const json = JSON.stringify(requestBody)
+  const stream = servertest(server, endpoint, {
+    method,
+    encoding: 'json',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': Buffer.byteLength(json)
+    }
+  }, callback)
+  stream.end(json)
+}
+
+function createGetRequest (server, endpoint, callback) {
+  servertest(server, endpoint, {
+    method: 'GET',
+    encoding: 'json'
+  }, callback)
+}
+
+function createDeleteRequest (server, endpoint, callback) {
+  servertest(server, endpoint, {
+    method: 'DELETE',
+    encoding: 'json'
+  }, callback)
+}
+
+module.exports = {
+  createTestRequest,
+  createGetRequest,
+  createDeleteRequest
+} 
